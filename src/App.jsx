50a46799d3b2e61b2d9d4a0bcfda792424ddf7ba
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-07 23:28:18 +0000

    Built websocket and connected

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
-
 class App extends Component {
 
   constructor(props) {
@@ -10,18 +9,7 @@ class App extends Component {
       this.createMessage = this.createMessage.bind(this);
       this.state = {
       currentUser: {name: "Bob"},
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-          id: 1
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: 2
-        }
-      ]
+      messages: []
     }
   }
 
@@ -36,14 +24,18 @@ componentDidMount() {
     // Calling setState will trigger a call to render() in App and all child components.
     this.setState({messages: messages})
   }, 3000);
+  this.socket = new WebSocket("ws://localhost:4000");
+
 }
 
 createMessage (event) {
   if (event.keyCode === 13) {
     console.log(event.target.value);
-    const newMessage = {id:4 , username:this.state.currentUser.name, content:event.target.value}
+    const newMessage = {id: 4 , username: this.state.currentUser.name, content: event.target.value}
     const messages = this.state.messages.concat(newMessage)
     this.setState({messages: messages})
+    this.socket.send(JSON.stringify(newMessage));
+    event.target.value = "";
   }
 }
 
