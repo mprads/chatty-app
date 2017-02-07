d3d1204b187bbd37b995e0e414551c9b9f82f387
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-07 17:42:45 +0000

    renders preloaded messages and michell

---
+++ src/Message.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+    console.log("Rendering <Message/>");
+    const messages = this.props.messages.map((message) =>
+      <div key={message.id} className="message">
+        <span className="message-username">{message.username}</span>
+        <span className="message-content">{message.content}</span>
+      </div>
+    );
+    return (
+      <div>{messages}</div>
+    );
+  }
+}
+export default Message;
